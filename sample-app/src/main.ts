7c0bf2cd1a3cdbd53991783f4a2c890915d6ff7d
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2021-04-19 21:43:21 +0900

    composition apiで書き換えられるように設定

--- sample-app/src/main.ts
+++ sample-app/src/main.ts
@@ -2,6 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+// 追加
+import VueCompositionAPI from '@vue/composition-api';
+Vue.use(VueCompositionAPI);
 
 Vue.config.productionTip = false;
 
